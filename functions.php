417087ec5376f54dda09f91afb7ac5b541d0727e
Luke Logan <llogan382@gmail.com>
2018-12-30 21:37:47 -0500

    side-left image gallery works

--- functions.php
+++ functions.php
@@ -12,11 +12,3 @@ require_once(TEMPLATEPATH . '/framework/BootsTrap.php');
 require_once(TEMPLATEPATH . '/theme_config/theme_includes/AJAX_CALLBACKS.php');
 
 
-add_action( 'wp_enqueue_scripts', 'ls_scripts_styles', 20 );
-
-function ls_scripts_styles() {
-	wp_enqueue_style( 'lightslidercss', get_stylesheet_directory_uri(). '/css/lightslider.css' , array(), '1.0.0', 'all' );
-	wp_enqueue_script( 'lightsliderjs', get_stylesheet_directory_uri() . '/js/lightslider.min.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( 'lightsliderinit', get_stylesheet_directory_uri() . '/js/lightslider-init.js', array( 'lightsliderjs' ), '1.0.0', true );
-}
-
