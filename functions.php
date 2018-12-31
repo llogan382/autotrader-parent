a0ec2c26839a0626caaab82d9f215d7ef90308ec
Luke Logan <llogan382@gmail.com>
2018-12-30 20:49:26 -0500

    adds lightbox to functions

--- functions.php
+++ functions.php
@@ -10,3 +10,11 @@
 
 require_once(TEMPLATEPATH . '/framework/BootsTrap.php');
 require_once(TEMPLATEPATH . '/theme_config/theme_includes/AJAX_CALLBACKS.php');
+
+
+function ls_scripts_styles() {
+	wp_enqueue_style( 'lightslidercss', get_stylesheet_directory_uri(). '/css/lightslider.css' , array(), '1.0.0', 'all' );
+	wp_enqueue_script( 'lightsliderjs', get_stylesheet_directory_uri() . '/js/lightslider.min.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'lightsliderinit', get_stylesheet_directory_uri() . '/js/lightslider-init.js', array( 'lightsliderjs' ), '1.0.0', true );
+}
+
--- offer-views/side-left.php
+++ offer-views/side-left.php
@@ -16,5 +16,29 @@
     }
 ?>
 <!-- offer left -->
+<?php 
 
+$images = get_field('lwd_vehicle_images');
+
+if( $images ): ?>
+    <div id="slider" class="flexslider">
+        <ul class="slides">
+            <?php foreach( $images as $image ): ?>
+                <li>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <p><?php echo $image['caption']; ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div id="carousel" class="flexslider">
+        <ul class="slides">
+            <?php foreach( $images as $image ): ?>
+                <li>
+                    <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
 <!--/ offer left -->
